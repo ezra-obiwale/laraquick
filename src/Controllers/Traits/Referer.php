38a6fa0535ba114ee114ce8f85b11429ec6986fe
Ezra Obiwale <contact@ezraobiwale.com>
2017-10-26 19:09:33 +0100

    Allowed referer to take on multiple urls at once

--- src/Controllers/Traits/Referer.php
+++ src/Controllers/Traits/Referer.php
@@ -4,14 +4,31 @@ namespace Laraquick\Controllers\Traits;
 
 trait Referer {
 
+    /**
+     * Verify that the referer's origin matches the origin of the given url(s)
+     *
+     * @param string|array $url If array, it would throw exception if the origin does not match any
+     * @throws \Exception
+     * @return void
+     */
     protected function verifyReferer($url)
     {
+        if (!is_array($url)) $url = [$url];
         $referer = request()->headers->get('referer');
         $refOrigin = $this->originFromUrl($referer);
-        $urlOrigin = $this->originFromUrl($url);
-        if ($refOrigin !== $urlOrigin) throw new \Exception($referer . ' is not a valid domain.');
+        foreach ($url as $urll) {
+            $urlOrigin = $this->originFromUrl($url);
+            if ($refOrigin == $urlOrigin) return;
+        }
+        throw new \Exception($referer . ' is not a valid domain.');
     }
 
+    /**
+     * Gets the origin from the url string
+     *
+     * @param string $url
+     * @return string
+     */
     protected function originFromUrl($url)
     {
         // remove protocol
