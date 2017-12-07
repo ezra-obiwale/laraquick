76930ed9092d7d6cc5e5e35bd5e26ad42c7edfa1
Ezra Obiwale <contact@ezraobiwale.com>
2017-12-07 10:22:56 +0100

    Added AsyncCall job

---
+++ src/Jobs/AsyncCall.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Laraquick\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+/**
+ * Call a function asynchronously. 
+ */
+class AsyncCall implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $callable;
+    protected $args;
+
+    /**
+     * Create a new job instance.
+     *
+	 * @param mixed $callable The function to call @see call_user_func_array()
+	 * @param array $args The arguments to pass to the called function
+     * @return void
+     */
+    public function __construct($callable, array $args)
+    {
+        $this->callable = $callable;
+        $this->args = $args;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        call_user_func_array($this->callable, $this->args);
+    }
+}
