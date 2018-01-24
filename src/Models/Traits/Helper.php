e83d1e4ed7647e660571daab40c5bfa406ccd600
Ezra Obiwale <contact@ezraobiwale.com>
2018-01-24 13:36:14 +0100

    Added without method to model helper trait

--- src/Models/Traits/Helper.php
+++ src/Models/Traits/Helper.php
@@ -2,7 +2,20 @@
 
 namespace Laraquick\Models\Traits;
 
+use Illuminate\Database\Eloquent\Builder;
+
 trait Helper {
+
+    /**
+     * A shortcut to withoutGlobalScope()
+     *
+     * @param string|array $attributes
+     * @return Builder
+     */
+    public function without($attributes)
+    {
+        return $this->withoutGlobalScope($attributes);
+    }
 	
     public function toArray()
     {
