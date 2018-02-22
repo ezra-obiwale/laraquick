2a78ad711e616a5cf7e86cbbd3b7e9eb8f878253
Ezra Obiwale <contact@ezraobiwale.com>
2018-02-22 17:33:41 +0100

    Ensured a model is passed to every other beforeResponse except index's

--- src/Controllers/Traits/PassThrough.php
+++ src/Controllers/Traits/PassThrough.php
@@ -109,11 +109,11 @@ trait PassThrough
         $this->responseStatusCode = Http::getStatusCode();
 
         $beforeMethod = 'before' . ucfirst($action) . 'Response';
-        if ($response = $this->$beforeMethod($resp)) return $response;
-
         if (!Http::hasErrors() && $action !== 'index') {
             $resp = (new Dud)->forceFill($resp);
         }
+
+        if ($response = $this->$beforeMethod($resp)) return $response;
         return response()->json($resp, Http::getStatusCode());
     }
 
