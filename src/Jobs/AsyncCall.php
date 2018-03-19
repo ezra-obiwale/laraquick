75d1514b2510782b472c52bedef7dafc2085bfd4
Ezra Obiwale <contact@ezraobiwale.com>
2018-03-19 15:39:23 +0100

    Updated AsyncCall job to take failedCallable

--- src/Jobs/AsyncCall.php
+++ src/Jobs/AsyncCall.php
@@ -7,6 +7,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Exception;
 
 /**
  * Call a function asynchronously. 
@@ -17,18 +18,22 @@ class AsyncCall implements ShouldQueue
 
     protected $callable;
     protected $args;
+    protected $failedCallable;
 
     /**
-     * Create a new job instance.
+     * Create a new job instance
      *
-	 * @param mixed $callable The function to call @see call_user_func_array()
-	 * @param array $args The arguments to pass to the called function
+     * @param callable|array $callable The function to call. @see call_user_func_array()
+     * @param array $args The arguments for the callable
+     * @param callable|array $failedCallable The function to call with the thrown exception if the job fails. @see call_user_func()
+     * 
      * @return void
      */
-    public function __construct($callable, array $args = [])
+    public function __construct($callable, array $args = [], $failedCallable = null)
     {
         $this->callable = $callable;
         $this->args = $args;
+        $this->failedCallable = $failedCallable;
     }
 
     /**
@@ -39,5 +44,18 @@ class AsyncCall implements ShouldQueue
     public function handle()
     {
         call_user_func_array($this->callable, $this->args);
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * @param  Exception  $exception
+     * @return void
+     */
+    public function failed(Exception $ex)
+    {
+        if ($this->failedCallable) {
+            call_user_func($this->failedCallable, $ex);
+        }
     }
 }
