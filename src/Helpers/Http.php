e060d594a7a8d7e13be188ad5cc2286210bab285
Ezra Obiwale <contact@ezraobiwale.com>
2018-06-11 18:28:00 +0100

    Added method to respond like normal laravel endpoint

--- src/Helpers/Http.php
+++ src/Helpers/Http.php
@@ -4,7 +4,6 @@ namespace Laraquick\Helpers;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Exception;
-use Log;
 
 /**
  * @method mixed DELETE($url, $_ = null)
@@ -21,13 +20,13 @@ class Http
 
     public static function client()
     {
-        if (!static::$client) static::$client = new Client;
-        return static::$client;
+        if (!self::$client) self::$client = new Client;
+        return self::$client;
     }
 
     private static function processResponse()
     {
-        return json_decode(strval(static::$response->getBody()), true);
+        return json_decode(strval(self::$response->getBody()), true);
     }
 
     private static function req($method, array $args)
@@ -42,7 +41,7 @@ class Http
         array_shift($args);
         $args[1]['http_errors'] = false;
 
-        static::$response = call_user_func_array([static::client(), $method], $args);
+        self::$response = call_user_func_array([static::client(), $method], $args);
 
         return static::processResponse();
     }
@@ -59,15 +58,20 @@ class Http
     }
 
     public static function getStatusCode() {
-        return static::$response ? static::$response->getStatusCode() : 0;
+        return self::$response ? self::$response->getStatusCode() : 0;
     }
 
     public static function rawResponse() {
-        return static::$response;
+        return self::$response;
     }
 
     public static function response() {
         return static::processResponse();
+    }
+
+    public static function respond()
+    {
+        return response()->json(static::response(), static::getStatusCode());
     }
 
     public static function __callStatic($method, $args)
