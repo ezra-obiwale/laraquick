c2642704224ce9c661f5a28b72c28fa2357e1daf
Ezra Obiwale <contact@ezraobiwale.com>
2018-06-27 09:29:33 +0100

    Fixed issue with fetching synced/detached items after the fact

--- src/Controllers/Traits/Attachable.php
+++ src/Controllers/Traits/Attachable.php
@@ -139,7 +139,7 @@ trait Attachable
             $model->$relation()->syncWithoutDetaching($this->prepareAttachItems($items, $model, $relation));
             return response()->json([
                 'status' => 'ok',
-                'data' => $model->$relation()->whereIn('id', $items)->get()
+                'data' => $model->$relation()->find($items)
             ]);
         }
         catch (\Exception $e) {
@@ -173,10 +173,11 @@ trait Attachable
         try {
             $items = request()->input($paramKey);
             $this->treatRelation($model, $relation);
+            $_items = $model->$relation()->find($items);
             $model->$relation()->detach($this->prepareDetachItems($items, $model, $relation));
             return response()->json([
                 'status' => 'ok',
-                'data' => $model->$relation()->whereIn('id', $items)->get()
+                'data' => $_items
             ]);
         }
         catch (\Exception $e) {
