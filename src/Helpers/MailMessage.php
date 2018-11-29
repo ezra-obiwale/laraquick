3a4e18039b5cb92ca8f695c5e3799befd37c7ac3
Ezra Obiwale <contact@ezraobiwale.com>
2018-11-29 14:06:05 +0100

    Added a MailMessage helper class

---
+++ src/Helpers/MailMessage.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Laraquick\Helpers;
+
+use Illuminate\Notifications\Messages\MailMessage as iMailMessage;
+
+class MailMessage extends iMailMessage {
+
+    protected $table = [];
+
+    public function setTableHeaders(array $headers) : self
+    {
+        $this->table['headers'] = $headers;
+        return $this;
+    }
+
+    public function addTableRow(array $rowData, int $rowIndex = null) : self
+    {
+        if ($rowIndex) {
+            $this->table['rows'][$rowIndex] = $rowData;
+        } else {
+            $this->table['rows'][] = $rowData;
+        }
+        return $this;
+    }
+
+    public function setTableFooters(array $footers) : self
+    {
+        $this->table['footers'] = $footers;
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function with($line)
+    {
+        if ($line instanceof Action) {
+            $this->action($line->text, $line->url);
+        } elseif (!$this->actionText && !count($this->table)) {
+            $this->introLines[] = $this->formatLine($line);
+        } else {
+            $this->outroLines[] = $this->formatLine($line);
+        }
+
+        return $this;
+    }
+
+    public function toArray() : array
+    {
+        return get_object_vars($this);
+    }
+
+}
+
