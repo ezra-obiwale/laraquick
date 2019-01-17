56721ef6a7e0e74ee22c82ab6f2b942dd2b5bf86
Ezra Obiwale <contact@ezraobiwale.com>
2019-01-17 10:47:12 +0100

    AsyncCall's second parameter is the arguments

--- src/Jobs/AsyncCall.php
+++ src/Jobs/AsyncCall.php
@@ -17,6 +17,7 @@ class AsyncCall implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $callable;
+    protected $arguments;
     protected $callback;
     protected $tags;
 
@@ -24,14 +25,16 @@ class AsyncCall implements ShouldQueue
      * Create a new job instance
      *
      * @param callable|array $callable The function to call. @see call_user_func_array()
+     * @param array $arguments The arguments to the callable. @see call_user_fun_array()
      * @param callable|array $callback The function to call with the result of the callable. In case of an exception, the second parameter will be the exception object. @see call_user_func_array()
      * @param array $tags The tags to attach to the job
      *
      * @return void
      */
-    public function __construct(callable $callable, callable $callback = null, array $tag = [])
+    public function __construct(callable $callable, array $arguments = [], callable $callback = null, array $tag = [])
     {
         $this->callable = $callable;
+        $this->arguments = $arguments;
         $this->callback = $callback;
         array_unshift($tags, 'async-call');
         $this->tags = $tags;
@@ -44,7 +47,7 @@ class AsyncCall implements ShouldQueue
      */
     public function handle()
     {
-        $result = call_user_func($this->callable);
+        $result = call_user_func_array($this->callable, $this->arguments);
         if ($this->callback) {
             call_user_func($this->callback, $result);
         }
